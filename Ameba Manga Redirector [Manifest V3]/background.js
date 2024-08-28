3971ec2a8614e5a2c38023f0832985dc91be7585
Neil Frydendal <60525040+nfrydendal@users.noreply.github.com>
2024-08-28 10:52:09 -0700

    Add files via upload

---
+++ Ameba Manga Redirector [Manifest V3]/background.js
@@ -0,0 +1,25 @@
+chrome.webNavigation.onCommitted.addListener(async (details) => {
+  // Define the base URL
+  const baseUrl = "https://stat.dokusho-ojikan.jp/";
+
+  // URL pattern without query parameters
+  const urlPattern = /^https:\/\/stat\.dokusho-ojikan\.jp\/[a-z0-9-]+\.jpg$/;
+
+  // Check if the URL starts with the base URL and doesn't match the target URL pattern
+  if (details.url.startsWith(baseUrl) && !urlPattern.test(details.url)) {
+    // Split the URL by '?'
+    const urlParts = details.url.split('?');
+
+    // The first part is the URL without the query parameters
+    const redirectUrl = urlParts[0];
+
+    // Redirect to the URL without query parameters
+    try {
+      await chrome.tabs.update(details.tabId, { url: redirectUrl });
+    } catch (error) {
+      console.error(`Error redirecting URL: ${error.message}`);
+    }
+  }
+}, {
+  url: [{ hostContains: "stat.dokusho-ojikan.jp" }]
+});
